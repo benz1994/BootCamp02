45caf8238cbba6b3d3eb15a2bb4b6b6f8114bff2
benz1994 <benzmichaela@yahoo.de>
2015-11-18 19:49:13 -0500

    smallest unit test

--- src/smallest.java
+++ src/smallest.java
@@ -6,7 +6,13 @@ public class smallest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		bootcamp02 test = new bootcamp02();
+		int [] numbers ={4,1, 0, 5, 7 , 10, 34, -1};
+		int minium =test.smallest(numbers);
+		assertEquals(-1,minium);
+		assertEquals(4,minium);
+		
 	}
 
 }
