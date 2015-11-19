fcd4105e595ce9bc569b153f0ed24897155e2dfe
benz1994 <benzmichaela@yahoo.de>
2015-11-18 19:21:26 -0500

    finished

--- src/bootcamp02.java
+++ src/bootcamp02.java
@@ -1,9 +1,154 @@
 
 public class bootcamp02 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		
 
 	}
+	// Task 1
+	public static int smallest( int[] smallest)
+	{
+	    int smallest_value = smallest[0];
+	    int current_value=0;
+		for(int i =1; i<smallest.length;i++)
+		{
+			
+			current_value = smallest[i];
+			if(smallest_value>current_value)
+			{
+				smallest_value =current_value;
+			}
+			
+		}
+		
+		return smallest_value;
+	}
+	//------------------------------
+	// Task 2
 
-}
+	public static int[] populateArray (int left, int right, int count)
+	{
+		int [] newArray = new int[count];
+		int j=0;
+		if(left>right)
+		{
+		for(int i =left;i>right+1;--i)
+		{ 
+			newArray[j] = i;
+			j++;
+		}
+		}
+		 else if(left<right)
+		{
+			
+			 for(int i =left;i<right+1;++i)
+				{
+				 newArray[j] = i;
+					j++;
+				}
+					
+				}
+		
+	   else
+	   {
+			
+			 for(int i =left;i<right+1;++i)
+				{
+				 newArray[j] = i;
+					j++;
+				}
+					
+				}
+		return newArray;
+		}	
+	// -----------------------------------
+	// Task3
+	public static double[] populateArray (double left, double right, int count)
+	{
+		double [] newArray = new double[count];
+		int j=0;
+		if(left>right)
+		{
+		for(double i =left;i>right+1;--i)
+		{ 
+			newArray[j] = i;
+			j++;
+		}
+		}
+		 else if(left<right)
+		{
+			
+			 for(double i =left;i<right+1;++i)
+				{
+				 newArray[j] = i;
+					j++;
+				}
+					
+				}
+		
+	   else
+	   {
+			
+			 for(double i =left;i<right+1;++i)
+				{
+				 newArray[j] = i;
+					j++;
+				}
+					
+				}
+		return newArray;
+		}	
+	// ---------------------------------
+	//Task4
+	public static int[] grow(int [] biggerArray)
+	{
+		int [] tmp= biggerArray;
+		 biggerArray = new int [2*biggerArray.length];
+		 for(int i =0; i<tmp.length;++i)
+		 {
+			 biggerArray[i] = tmp[i];
+		 }
+		 return biggerArray;
+	}
+	//--------------------
+	// Task 5
+	public static int [] shrink (int[] smallerArray)
+	{
+		int [] tmp = smallerArray;
+		smallerArray = new int [smallerArray.length/2];
+		 for(int i =0; i<smallerArray.length;++i)
+		 {
+			 smallerArray[i] = tmp[i];
+		 }
+		 return smallerArray;
+		
+	}
+	//----------------------------------
+	// Task 6
+	public static double [] grow(double [] biggerArray)
+	{
+		double tmp[] = biggerArray;
+		biggerArray = new double[biggerArray.length*2];
+		 for(int i =0; i<tmp.length;++i)
+		 {
+			 biggerArray[i] = tmp[i];
+		 }
+		 return biggerArray;
+	}
+	// ----------
+	// Task 7
+	public static double [] shrink (double [] smallerArray)
+	{
+		double [] tmp = smallerArray;
+		smallerArray = new double [smallerArray.length/2];
+		 for(int i =0; i<smallerArray.length;++i)
+		 {
+			 smallerArray[i] = tmp[i];
+		 }
+		 return smallerArray;
+	}
+	}
+		
+	
+
